2aa8137dced807dfa2e312f3847e1858ed50cb37
tadhg.m <tadhg.moore@dkit.ie>
2020-11-25 16:51:59 -0500

    Remove NLOAD

--- create_npz_inputs.R
+++ create_npz_inputs.R
@@ -1,7 +1,6 @@
 # Create npz inputs
 
-create_npz_inputs <- function(time, PAR = NULL, swr = NULL, temp = NULL, nload = NULL,
-                              out_file = "PAR_TEMP_NLOAD_inputs.csv") {
+create_npz_inputs <- function(time, PAR = NULL, swr = NULL, temp = NULL) {
   
   # year <- lubridate::year(time[1])
   ndays <- round(as.numeric(difftime(time, time[1], units = "days")))
@@ -20,17 +19,9 @@ create_npz_inputs <- function(time, PAR = NULL, swr = NULL, temp = NULL, nload =
   } else {
     TEMP <- temp
   }
-
-  if(is.null(nload)) {
-    
-    NLOAD <- rep(NA, length(ndays))
-    for(i in 1:length(ndays)) {
-      NLOAD[i] <- ((1+1*sin(2*pi*ndays[i]/365-1.4)) * 0.1) + rnorm(1, 0.05, 0.01)
-    }
-  } 
   
 
-  out = data.frame(Day = ndays, PAR = PAR, TEMP = TEMP, NLOAD = NLOAD)
+  out = data.frame(Day = ndays, PAR = PAR, TEMP = TEMP)
 
   
   #Read to a .csv file
