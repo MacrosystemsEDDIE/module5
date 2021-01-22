e535861378b5b3d3f14e6028ed2e2ed00f81a058
tadhg-moore <tadhgm@vt.edu>
2021-01-22 12:25:17 -0500

    Add guide to running Shiny locally

---
+++ install_packages.R
@@ -0,0 +1,38 @@
+# If running the Shiny app locally you can install all the necessary packages here
+# Watch out for errors in package installation. Most can be avoided using the most up-to-date version of R (4.0.3 as of 2021-01-22).
+# Updating of current R packages on your system is recommended.
+
+install.packages("shiny")
+install.packages("shinycssloaders")
+install.packages("shinyjs")
+install.packages("shinydashboard")
+install.packages("htmltools")
+install.packages("sf")
+install.packages("leaflet")
+install.packages("ncdf4")
+install.packages("reshape")
+install.packages("sortable")
+install.packages("tinytex")
+install.packages("rvest")
+install.packages("LakeMetabolizer")
+install.packages("rLakeAnalyzer")
+install.packages("DT")
+install.packages("rintrojs")
+install.packages("stringr")
+install.packages("tidyr")
+install.packages("readr")
+install.packages("plyr")
+install.packages("lubridate")
+install.packages("zoo")
+install.packages("deSolve")
+install.packages("reshape")
+install.packages("reshape2")
+install.packages("RColorBrewer")
+install.packages("ggplot2")
+install.packages("plotly")
+install.packages("ggpubr")
+install.packages("rmarkdown")
+
+remotes::install_github('yonicd/slickR') # removed from CRAN - now only on GitHub
+
+# end
