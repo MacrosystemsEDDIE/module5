1bd326fc92bd3ed8a97625cd47398f983552dbe8
tadhg-moore <tadhgm@vt.edu>
2021-02-03 16:56:01 -0500

    Updated test

--- test_NP_model.R
+++ test_NP_model.R
@@ -23,6 +23,8 @@ source("get_html.R")
 source("create_npz_inputs.R")
 source("NP_model.R")
 source("NP_model_no_temp.R")
+source("NP_model_no_temp_no_light.R")
+source("NP_model_no_light.R")
 source("textAreaInput2.R")
 
 # Load in sp format with coordinates
@@ -104,8 +106,8 @@ parms <- c(
   Chl_Nratio = 1, #mg chl (mmolN)-1
   Q10 = 2,  #unitless
   addTEMP = 0, # added to temperature
-  scaleNLOAD = 1 # multiplier for N loading
-  
+  scaleNLOAD = 1, # multiplier for N loading
+  refTEMP = 20
 )  
 
 # Initial conditions for NPZ
@@ -115,77 +117,86 @@ yini <- c(
   # DETRITUS = 1, #mmolN m-3
   DIN = 9) #mmolN m-3
 
+# Load parameters
+site_parms <- read.csv("data/params_site_NP_model.csv", fileEncoding = "UTF-8-BOM")
+site_yini <- read.csv("data/yini_sites_NP_model.csv", fileEncoding = "UTF-8-BOM")
+
 
 
 ui <- fluidPage(
-  column(6,
-         DTOutput("table01"),
-         h3("Run Model"),
-         actionButton("run_mod_ann",
-                      label = div("Run Model",
-                                  icon("running")),
-                      width = "60%"),
-         checkboxInput("add_obs", "Add observations"),
-         checkboxGroupInput("mod_sens", "Switch on or off the temperature sensitivity:",
-                            choices = list("Temperature")),
-         plotlyOutput("mod_ann_plot"),
-         plotlyOutput("mod_phyto_plot")
-         ),
-  column(3,
-         # wellPanel(
-         # h3("Inputs"),
-         # checkboxGroupInput("mod_sens", "Switch on or off the temperature sensitivity:",
-         #                    choices = list("Temperature")),
-         h3("Initial conditions"),
-         p("Adjust these to values that are within reasonable ranges as seen in the 'Objective 2 - Explore data' tab. Phytoplankton corresponds to chlorophyll-a concentrations and nutrients corresponds to Dissolved Inorganic Nitrogen."),
-         p(tags$b("Phytoplankton")),
-         # slider labels: https://stackoverflow.com/questions/40415471/sliderinput-max-min-text-labels
-         sliderInput("phy_init", label = div(style='width:300px;', div(style='float:left;', img(src = "phyto.png", height = "50px", width = "50px")),
-                                             div(style='float:right;', img(src = "phytos.png", height = "50px", width = "50px", align = "right"))),
-                     min = 0.1, max = 40, step = 0.1, value = 2),
-         # p(tags$b("Zooplankton")),
-         # sliderInput("zoo_init", label = div(style='width:300px;', div(style='float:left;', img(src = "zoop.png", height = "50px", width = "50px")),
-         #                                     div(style='float:right;', img(src = "zoops.png", height = "50px", width = "50px", align = "right"))),
-         #             min = 0.1, max = 5, step = 0.1, value = 0.4),
-         p(tags$b("Nutrients")),
-         sliderInput("nut_init", label = div(style='width:300px;', div(style='float:left;', img(src = "nutri.png", height = "50px", width = "50px")),
-                                             div(style='float:right;', img(src = "nutris.png", height = "50px", width = "50px", align = "right"))),
-                     min = 0.01, max = 20, step = 0.1, value = 3)
-         # )
-         ,
-         # wellPanel(
+  fluidRow(
+    column(6,
+           DTOutput("table01"),
+           h3("Run Model"),
+           actionButton("run_mod_ann",
+                        label = div("Run Model",
+                                    icon("running")),
+                        width = "60%"),
+           checkboxInput("add_obs", "Add observations", value = TRUE),
+           checkboxGroupInput("mod_sens", "Switch on or off the temperature sensitivity:",
+                              choices = list("Surface water temperature", "Underwater light (uPAR)"))
+    )
   ),
-  column(3,
-         h3("Parameters"),
-         h4(tags$b("Phytoplankton parameters")),
-         # p(tags$em("Grazing")),
-         # sliderInput("graz_rate", label = div(style='width:300px;', 
-         #                                      div(style='float:left;', 'Eat less'), 
-         #                                      div(style='float:right;', 'Eat more')),
-         #             min = 0.2, max = 1.6, value = 1.2, step = 0.1),
-         p(tags$em("Mortality")),
-         sliderInput("mort_rate", label = div(style='width:300px;', 
-                                              div(style='float:left;', 'Lower death'), 
-                                              div(style='float:right;', 'Higher death')),
-                     min = 0.01, max = 1, value = 0.3, step = 0.01),
-         sliderInput("kspar", "KSpar", min = 60, max = 240, value = 120, step = 10),
-         sliderInput("ksdin", "KSdin", min = 0, max = 2, value = 0.5, step = 0.01),
-         # sliderInput("ksphyto", "KSphyto", min = 0, max = 2, value = 1, step = 0.01),
-         sliderInput("miner_rate", "Mineralization", min = 0, max = 4, value = 0.1, step = 0.01),
-         sliderInput("q10", "q10", min = 0.01, max = 10, value = 2, step = 0.01),
-         h4(tags$b("Phytoplankton parameters")),
-         p(tags$em("Uptake")),
-         sliderInput("nut_uptake", label = div(style='width:300px;', 
-                                               div(style='float:left;', 'Low uptake'), 
-                                               div(style='float:right;', 'High uptake')),
-                     min = 0, max = 3, value = 0.8, step = 0.1)
-         
-  ),
-  column(5,
-         # h3("Primary Productivity"),
-         # wellPanel(
-         #   plotlyOutput("mod_ann_plot")
-         # )
+  fluidRow(
+    column(6,
+           plotlyOutput("mod_ann_plot"),
+           plotlyOutput("mod_phyto_plot"),
+           plotlyOutput("input_plot")
+           ),
+    column(3,
+           # wellPanel(
+           # h3("Inputs"),
+           # checkboxGroupInput("mod_sens", "Switch on or off the temperature sensitivity:",
+           #                    choices = list("Surface water temperature")),
+           h3("Initial conditions"),
+           p("Adjust these to values that are within reasonable ranges as seen in the 'Objective 2 - Explore data' tab. Phytoplankton corresponds to chlorophyll-a concentrations and nutrients corresponds to Dissolved Inorganic Nitrogen."),
+           p(tags$b("Phytoplankton")),
+           # slider labels: https://stackoverflow.com/questions/40415471/sliderinput-max-min-text-labels
+           sliderInput("phy_init", label = div(style='width:300px;', div(style='float:left;', img(src = "phyto.png", height = "50px", width = "50px")),
+                                               div(style='float:right;', img(src = "phytos.png", height = "50px", width = "50px", align = "right"))),
+                       min = 0.1, max = 40, step = 0.1, value = 2),
+           # p(tags$b("Zooplankton")),
+           # sliderInput("zoo_init", label = div(style='width:300px;', div(style='float:left;', img(src = "zoop.png", height = "50px", width = "50px")),
+           #                                     div(style='float:right;', img(src = "zoops.png", height = "50px", width = "50px", align = "right"))),
+           #             min = 0.1, max = 5, step = 0.1, value = 0.4),
+           p(tags$b("Nutrients")),
+           sliderInput("nut_init", label = div(style='width:300px;', div(style='float:left;', img(src = "nutri.png", height = "50px", width = "50px")),
+                                               div(style='float:right;', img(src = "nutris.png", height = "50px", width = "50px", align = "right"))),
+                       min = 0.01, max = 5, step = 0.01, value = 0.1)
+           # )
+           ,
+           # wellPanel(
+    ),
+    column(3,
+           h3("Parameters"),
+           h4(tags$b("Phytoplankton parameters")),
+           # p(tags$em("Grazing")),
+           # sliderInput("graz_rate", label = div(style='width:300px;', 
+           #                                      div(style='float:left;', 'Eat less'), 
+           #                                      div(style='float:right;', 'Eat more')),
+           #             min = 0.2, max = 1.6, value = 1.2, step = 0.1),
+           sliderInput("nut_uptake", label = div(style='width:300px;', 
+                                                 div(style='float:left;', 'Low uptake'), 
+                                                 div(style='float:right;', 'High uptake')),
+                       min = 0, max = 1, value = 0.8, step = 0.01),
+           p(tags$em("Mortality")),
+           sliderInput("mort_rate", label = div(style='width:300px;', 
+                                                div(style='float:left;', 'Lower death'), 
+                                                div(style='float:right;', 'Higher death')),
+                       min = 0.01, max = 1, value = 0.3, step = 0.01),
+           sliderInput("kspar", "KSpar", min = 0, max = 600, value = 120, step = 1),
+           sliderInput("ksdin", "KSdin", min = 0, max = 20, value = 0.5, step = 0.01),
+           sliderInput("chla_nratio", label = "Ratio of Chlorophyll to Nitrogen", 
+                       min = 0.01, max = 3, step = 0.01, value = 1),
+           # sliderInput("ksphyto", "KSphyto", min = 0, max = 2, value = 1, step = 0.01),
+           # sliderInput("miner_rate", "Mineralization", min = 0, max = 4, value = 0.1, step = 0.01),
+           sliderInput("q10", "q10", min = 0.01, max = 10, value = 2, step = 0.01),
+           sliderInput("refT", "refT", min = 1, max = 30, value = 20, step = 1)
+           # h4(tags$b("Phytoplankton parameters")),
+           # p(tags$em("Uptake")),
+           
+           
+    )
   )
 )
 
@@ -200,6 +211,8 @@ server <- function(input, output, session) {
   observeEvent(input$table01_rows_selected, {
     row_selected = neon_sites[input$table01_rows_selected, ]
     siteID <<- neon_sites$siteID[input$table01_rows_selected]
+    upd_parms <- site_parms[site_parms$site == siteID, -1]
+    
   })
   
   #* Run eco-model ----
@@ -254,45 +267,51 @@ server <- function(input, output, session) {
     parms[2] <- as.numeric(input$kspar)
     parms[3] <- as.numeric(input$ksdin)
     # parms[5] <- as.numeric(input$ksphyto)
-    parms[9] <- as.numeric(input$miner_rate)
+    parms[10] <- as.numeric(input$chla_nratio)
     parms[11] <- as.numeric(input$q10)
+    parms[14] <- as.numeric(input$refT)
     # parms[4] <- as.numeric(input$graz_rate)
     parms[7] <- as.numeric(input$mort_rate)
     
-    inputs <- create_npz_inputs(time = npz_inp[, 1], PAR = npz_inp[, 2], temp = npz_inp[, 3])
+    npz_inputs <- create_npz_inputs(time = npz_inp[, 1], PAR = npz_inp[, 2], temp = npz_inp[, 3])
     
+    print(summary(npz_inputs))
     
     # Alter Initial conditions
-    yini[1] <- input$phy_init
-    # yini[2] <- input$zoo_init
-    yini[2] <- input$nut_init
+    yini[1] <- input$phy_init * 0.016129
+    yini[2] <- input$nut_init * 16.129
     
-    res <- matrix(NA, nrow = length(times), ncol = 4)
-    colnames(res) <- c("time", "Chla", "Phytoplankton", "Nutrients")
+    res <- matrix(NA, nrow = length(times), ncol = 3)
+    colnames(res) <- c("time", "Phytoplankton", "Nutrients")
     res[, 1] <- times
-    res[1, -1] <- c(yini[1], yini)
+    res[1, -1] <- c(yini)
     
-    # yini <- c(2,9)
-    # Looped model version
     for(i in 2:length(times)) {
       
-      if(!("Temperature" %in% input$mod_sens)) {
-        out <- as.matrix(deSolve::ode(y = yini, times = times[(i-1):i], func = NP_model_noT,
-                                      parms = parms, method = "ode45", inputs = inputs))
-      } else {
+      if(all(c("Surface water temperature", "Underwater light (uPAR)") %in% input$mod_sens)) {
         out <- as.matrix(deSolve::ode(y = yini, times = times[(i-1):i], func = NP_model,
-                                      parms = parms, method = "ode45", inputs = inputs))
+                                      parms = parms, method = "ode45", inputs = npz_inputs))
+      } else if((c("Underwater light (uPAR)") %in% input$mod_sens)) {
+        out <- as.matrix(deSolve::ode(y = yini, times = times[(i-1):i], func = NP_model_noT,
+                                      parms = parms, method = "ode45", inputs = npz_inputs))
+      } else if((c("Surface water temperature") %in% input$mod_sens)) {
+        out <- as.matrix(deSolve::ode(y = yini, times = times[(i-1):i], func = NP_model_noPAR,
+                                      parms = parms, method = "ode45", inputs = npz_inputs))
+      } else {
+        out <- as.matrix(deSolve::ode(y = yini, times = times[(i-1):i], func = NP_model_noTPAR,
+                                      parms = parms, method = "ode45", inputs = npz_inputs))
       }
-      
-      res[i, -1] <- out[2, c(4, 2, 3)]
+      # print((out[2, ]))
+      res[i, -1] <- out[2, c(2, 3)]
+      # print(res[i, ])
       yini <- out[2, c(2:3)]
       
     }
     res <- as.data.frame(res)
     res$time <- npz_inp$Date
+    res$Chla <- (res$Phytoplankton * 62) # Convert from mmol/m3 to ug/L # * 4.97 + 1.58
+    res$Nutrients <- res$Nutrients * 0.062 # Convert from mmol/m3 to mg/L
     res <- res[, c("time", "Chla", "Nutrients")]
-    # res[ ,-1] <- ((res[ ,-1] / 1000) * 14) * 100
-    
     return(res)
     
     
@@ -321,6 +340,9 @@ server <- function(input, output, session) {
     
     if(siteID == "PRLA") {
       chla <- chla[(chla[, 1] > as.POSIXct("2019-07-06")), ]
+    }
+    if(siteID == "SUGG") {
+      chla <- chla[(chla[, 1] > as.POSIXct("2019-04-10")), ]
     }
     if(siteID == "PRPO") {
       chla <- chla[(chla[, 2] < 40), ]
@@ -360,8 +382,9 @@ server <- function(input, output, session) {
       need(input$run_mod_ann > 0, "Click 'Run Model'")
     )
     
+    print(summary(mod_run1()))
     xlims <- range(mod_run1()[, 1])
-    mlt <- reshape2::melt(mod_run1()[, -c(2)], id.vars = 1)
+    mlt <- reshape2::melt(mod_run1()[, -c(2, 4)], id.vars = 1)
     ylims <- c(0, max(mlt[, 3]))
     
     validate(
@@ -394,6 +417,65 @@ server <- function(input, output, session) {
       scale_color_manual(values = cols[3:8])
     return(ggplotly(p, dynamicTicks = TRUE))
     
+  })
+  
+  
+  # Plot input variables
+  output$input_plot <- renderPlotly({
+    
+    validate(
+      need(!is.null(input$table01_rows_selected), "Please select a site on the 'Activity A' tab - Objective 1")
+    )
+    validate(
+      need(input$run_mod_ann > 0, "Click 'Run Model'")
+    )
+    
+    par_file <- file.path("data", paste0(siteID, "_uPAR_micromolesPerSquareMeterPerSecond.csv"))
+    wtemp_file <- file.path("data", paste0(siteID, "_wtemp_celsius.csv"))
+    # if(file.exists(par_file))
+    
+    par <- read.csv(par_file)
+    par[, 1] <- as.POSIXct(par[, 1], tz = "UTC")
+    yr <- lubridate::year(par[, 1])
+    par <- par[yr == 2019, ] # Subset data to 2019
+    if(sum(is.na(par[, 2])) > 0) {
+      idx <- which(!is.na(par[, 2]))
+      sta <- idx[1]
+      stp <- idx[length(idx)]
+      par[sta:stp, 2] <- zoo::na.approx(par[sta:stp, 2])
+      par[1:sta, 2] <- par[sta, 2]
+      par[stp:nrow(par), 2] <- par[stp, 2]
+    }
+    par[(par[, 2] < 0), 2] <- 0
+    
+    wtemp <- read.csv(wtemp_file)
+    stemp <- wtemp[wtemp[, 2] == min(wtemp[, 2]), c(1, 3)]
+    stemp[, 1] <- as.POSIXct(stemp[, 1], tz = "UTC")
+    yr <- lubridate::year(stemp[, 1])
+    stemp <- stemp[yr == 2019, ] # Subset data to 2019
+    if(sum(is.na(stemp[, 2])) > 0) {
+      idx <- which(!is.na(stemp[, 2]))
+      sta <- idx[1]
+      stp <- idx[length(idx)]
+      stemp[sta:stp, 2] <- zoo::na.approx(stemp[sta:stp, 2])
+      stemp[1:sta, 2] <- stemp[sta, 2]
+      stemp[stp:nrow(stemp), 2] <- stemp[stp, 2]
+    }
+    
+    npz_inp <- merge(par, stemp, by = 1)
+    npz_inp[, 1] <- as.POSIXct(npz_inp[, 1], tz = "UTC")
+    times <- 1:nrow(npz_inp)
+    
+    npz_inputs <- create_npz_inputs(time = npz_inp[, 1], PAR = npz_inp[, 2], temp = npz_inp[, 3])
+    npz_inputs$Day <- npz_inp$Date
+    
+    mlt <- reshape::melt(npz_inputs, id.vars = 1)
+    
+    p <- ggplot() +
+      geom_line(data = mlt, aes(Day, value)) +
+      facet_wrap(~variable, scales = "free_y", ncol = 1) +
+      geom_hline(yintercept = 0)
+    return(ggplotly(p, dynamicTicks = TRUE))
   })
   
 }
